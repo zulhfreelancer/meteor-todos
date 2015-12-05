62fd8f98565383b480617429dedbf3b2b931af10
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2015-12-05 15:48:17 +0800

    Create todos CRUD

--- todos.js
+++ todos.js
@@ -5,8 +5,37 @@ if (Meteor.isClient) {
   // template helper
   Template.main.helpers({
     todos: function(){
-      return Todos.find();
+      return Todos.find({}, {sort: {createdAt: -1 }});
     }
+  });
+
+  Template.main.events({
+  	"submit .new-todo": function(event){
+  		var text = event.target.text.value;
+  		// --------------------- ^ input `name` attribute
+  		
+  		Todos.insert({
+  			text: text,
+  			createdAt: new Date()
+  		});
+
+  		// clear form
+  		event.target.text.value = "";
+
+  		return false; // prevent form default
+  	},
+  	"click .toggle-checked": function(){
+  		Todos.update(this._id, {
+  			$set: {checked: !this.checked}
+  			// let say the checkbox in the form is unchecked (false / not done)
+  			// when we click it, we toggle it using `!` to make it true / done 
+  		});
+  	},
+  	"click .delete-todo": function(){
+  		if (confirm("Are you sure to delete this todo?")) {
+  			Todos.remove(this._id);
+  		};
+  	}
   });
 }
 
